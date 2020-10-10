328b8567dd5cadacee73603982a0b82306950d3d
Bruno Hashimoto <bg.muniz1@gmail.com>
2020-10-10 14:40:56 -0400

    Add files via upload

---
+++ autoclick.py
@@ -0,0 +1,14 @@
+import webbrowser
+import time
+
+numtabs = 90000000000000000000000000000
+numquit = 0
+
+print ('O programa irá iniciar em ' + time.ctime())
+print ('Bem vindo! Bruno Hashimoto')
+link = input('Digite o link ')
+
+while(numquit  numtabs)
+    time.sleep(1)
+    webbrowser.open(link)
+    numquit = numquit + 1
